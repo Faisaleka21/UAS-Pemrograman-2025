8c8c3569bdd6df0939a7f73ae83693201bc49a24
Faisaleka21 <faisalekanurirawan21@gmail.com>
2025-07-15 14:09:03 +0700

    okkk

--- user/tentang.php
+++ user/tentang.php
@@ -15,7 +15,7 @@ requireLogin('petani');
 
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
+ 
   <!-- Tailwind -->
   <script src="https://cdn.tailwindcss.com"></script>
   <script>
